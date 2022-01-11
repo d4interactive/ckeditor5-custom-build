05bd061d827be9814bcb5773ed4b1de3a7e34ed7
seeratawan01 <seeratsdsking@gmail.com>
2022-01-11 16:10:25 +0500

    Fixed: Link

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -178,6 +178,23 @@ Editor.defaultConfig = {
 			'tableProperties', 'tableCellProperties'
 		]
 	},
+	link: {
+
+		defaultProtocol: 'http://',
+
+		// Let the users control the "download" attribute of each link.
+		decorators: {
+			openInNewTab: {
+				mode: 'manual',
+				label: 'Open in a new tab',
+				defaultValue: true,			// This option will be selected by default.
+				attributes: {
+					target: '_blank',
+					rel: 'noopener noreferrer'
+				}
+			}
+		}
+	},
 	extraPlugins: [
 		function (editor) {
 			// Allow <iframe> elements in the model.
--- src/custom/dragWidget/dragWidgetEditing.js
+++ src/custom/dragWidget/dragWidgetEditing.js
@@ -3,7 +3,7 @@ import { toWidget, viewToModelPositionOutsideModelElement } from '@ckeditor/cked
 import Widget from '@ckeditor/ckeditor5-widget/src/widget';
 import UpcastWriter from '@ckeditor/ckeditor5-engine/src/view/upcastwriter';
 
-import InsertDragBoxCommand from './dragWidgetCommand';imageBlock
+import InsertDragBoxCommand from './dragWidgetCommand';
 
 export default class DragWidgetEditing extends Plugin {
 
